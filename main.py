99458e08237e49bccba3fc1edd8a007a32787b07
Dompurrr <55359826+Dompurrr@users.noreply.github.com>
2021-12-04 11:06:02 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import telebot
+bot = telebot.TeleBot('5004146707:AAHXsREczeGn1xpICEgXLJGaalDh4-mZaMU')
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Ку")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Не помогу, брат...")
+    else:
+        bot.send_message(message.from_user.id, "Не понял тебя, брат.")
+
+bot.polling(none_stop=True, interval=0)
